7d0703fa6ee20a3b332437bfcf9559f1cd0d60c0
bohdan protasov <bogdan2148777@gmail.com>
2025-12-24 13:52:01 +0100

    refactor session creation logic + refactor refresh

--- backend/session_service/routes/session.py
+++ backend/session_service/routes/session.py
@@ -18,31 +18,37 @@ def generate_cookie(response: Response, session_id: str):
         path="/"
     )
 
+async def session_set_redis(session_id: str):
+        created_at = int(time.time())
+        await redis_client.set(
+            key=session_id,
+            value={"created_at": created_at},
+            ttl=30 * 24 * 3600,
+        )
+
 
 
 @router.post("/session_create", status_code=status.HTTP_201_CREATED)
 async def create_session(response: Response):
     session_id = (generate_uuid())
-    created_at = int(time.time())
     generate_cookie(response, session_id)
-    await redis_client.set(
-        key=session_id,
-        value={"created_at": created_at},
-        ttl=30*24*3600,
-    )
+    await session_set_redis(session_id)
     return {"status":"created","session_id": session_id}
 
 
 
 @router.post("/session_refresh", status_code=status.HTTP_200_OK)
-async def session_refresh(request: Request):
+async def session_refresh(request: Request , response: Response):
     session_id = request.cookies.get("session")
     if session_id is None:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED ,detail="No session cookie")
-    await redis_client.expire(
-        key =session_id ,
-        ttl=30*24*3600,
-    )
+    exists = await redis_client.exists(session_id)
+    if not exists:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid or expired session")
+    new_session_id = generate_uuid()
+    generate_cookie(response, new_session_id)
+    await redis_client.delete(session_id)
+    await session_set_redis(new_session_id)
     return {"status":"refreshed"}
 
 @router.post("/session_validate", status_code=status.HTTP_200_OK)
